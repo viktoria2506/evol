fb82c470ae863c74c5acbbe53fde78196d6e4be5
Viktoriya Chernookaya <32044161+viktoria2506@users.noreply.github.com>
2022-02-17 13:40:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,28 +13,49 @@ enum TypeAlgo {
 };
 
 const int REPEAT = 80;
+/*
+void runAlgo ( TypeGraph typeGraph, int size, TypeAlgo typeAlgo) {
+    vector<int> resultRls = vector<int> (REPEAT, 0);
+    vector<int> resultOnePlusOne = vector<int> (REPEAT, 0);
+    vector<int> resultLambda = vector<int> (REPEAT, 0);
 
-void runAlgo (Graph* graph, TypeGraph typeGraph, int size, TypeAlgo typeAlgo) {
+    int sumRls = 0;
+    int sumOnePlusOne = 0;
+    int sumLambda = 0;
 
-    vector<int> result = vector<int> (REPEAT, 0);
+    int maxRls = 0;
+    int maxOnePlusOne = 0;
+    int maxLambda = 0;
 
-    int sum = 0;
-    int max = 0;
-    int min = 100000000;
+    int minRls = 100000000;
+    int minOnePlusOne = 100000000;
+    int minLambda = 100000000;
 
     for (int i = 0; i < REPEAT; i++) {
-        if (typeAlgo == RLS) result[i] = graph->RLS(1);
-        else if (typeAlgo == ONEPLUSONE) result[i] = graph->onePlusOneAlgorithm(1);
-        else if (typeAlgo == LAMBDA) result[i] = graph->lambdaAlgorithm(1);
+        auto *graph = new Graph(size, typeGraph);
 
-        sum += result[i];
-        if (result[i] > max) max = result[i];
-        if (result[i] < min) min = result[i];
+        if (typeAlgo == RLS) resultRls[i] = graph->RLS(1);
+        else if (typeAlgo == ONEPLUSONE) resultOnePlusOne[i] = graph->onePlusOneAlgorithm(1);
+        else if (typeAlgo == LAMBDA) resultLambda[i] = graph->lambdaAlgorithm(1);
 
-        graph->reset();
+        sumRls += resultRls[i];
+        sumOnePlusOne += resultOnePlusOne[i];
+        sumLambda += resultLambda[i];
+
+        if (resultRls[i] > maxRls) maxRls = resultRls[i];
+        if (resultOnePlusOne[i] > maxOnePlusOne) maxOnePlusOne = resultOnePlusOne[i];
+        if (resultLambda[i] > maxLambda) maxLambda = resultLambda[i];
+
+        if (resultRls[i] < minRls) minRls = resultRls[i];
+        if (resultOnePlusOne[i] < minOnePlusOne) minOnePlusOne = resultOnePlusOne[i];
+        if (resultLambda[i] < minLambda) minLambda = resultLambda[i];
+
+        //graph->reset();
     }
 
-    double average = (double) sum / REPEAT;
+    double averageRls = (double) sumRls / REPEAT;
+    double averageOnePlusOne = (double) sumOnePlusOne / REPEAT;
+    double averageLambda = (double) sumLambda / REPEAT;
 
     ofstream out("/Users/viktoria/CLionProjects/evol/out", ios_base::app);
     out << "результаты:\n";
@@ -59,7 +80,10 @@ void runAlgo (Graph* graph, TypeGraph typeGraph, int size, TypeAlgo typeAlgo) {
     }
 
     out << "время работы: ";
-    for (int i =0; i < REPEAT; i++) {
+    if (sumRls > 0) {
+
+    }
+    for (int i = 0; i < REPEAT; i++) {
         out << result[i] << ' ';
     }
     out << '\n';
@@ -67,14 +91,86 @@ void runAlgo (Graph* graph, TypeGraph typeGraph, int size, TypeAlgo typeAlgo) {
 
     out << "среднее время работы - " << average << '\n';
     out << "нормированное по размеру задачи - " << (double) average / size << '\n';
-}
+    out << "----------------------------\n";
+}*/
 
-void run (TypeGraph typeGraph, int size, vector<TypeAlgo> typesAlgo) {
-    auto *graph = new Graph(size, typeGraph);
+/*void run (TypeGraph typeGraph, int size, vector<TypeAlgo> typesAlgo) {
+    //auto *graph = new Graph(size, typeGraph);
 
     for (auto & i : typesAlgo) {
-        runAlgo(graph, typeGraph, size, i);
+        //auto *graph = new Graph(size, typeGraph);
+
+        runAlgo(typeGraph, size, i);
     }
+
+}*/
+
+void run (TypeGraph typeGraph, int size, vector<TypeAlgo> typesAlgo) {
+    vector<vector<int>> result = vector<vector<int>> (typesAlgo.size(), vector<int>(REPEAT, 0));
+    vector<int> sum = vector<int>(typesAlgo.size(), 0);
+    vector<int> max = vector<int>(typesAlgo.size(), 0);
+    vector<int> min = vector<int>(typesAlgo.size(), 100000000);
+
+    for (int i = 0; i < REPEAT; i++) {
+        auto *graph = new Graph(size, typeGraph);
+
+        for (int j = 0; j < typesAlgo.size(); j++) {
+            if (typesAlgo[j] == RLS) result[j][i] = graph->RLS(1);
+            else if (typesAlgo[j] == ONEPLUSONE) result[j][i] = graph->onePlusOneAlgorithm(1);
+            else if (typesAlgo[j] == LAMBDA) result[j][i] = graph->lambdaAlgorithm(1);
+
+            graph->reset();
+
+            sum[j] += result[j][i];
+            if (result[j][i] > max[j]) max[j] = result[j][i];
+            if (result[j][i] < min[j]) min[j] = result[j][i];
+        }
+
+    }
+
+    vector<double> average = vector<double>(typesAlgo.size(), 0.0);
+
+    for (int j = 0; j < typesAlgo.size(); j++) {
+        average[j] = (double) sum[j] / REPEAT;
+    }
+
+    ofstream out("/Users/viktoria/CLionProjects/evol/out", ios_base::app);
+    out << "результаты:\n";
+    out << "размер графа - " << size << '\n';
+    out << "тип графа - ";
+
+    if (typeGraph == KN) {
+        out << "KN\n";
+    } else if (typeGraph == KNN) {
+        out << "KNN\n";
+    } else if (typeGraph == RANDOM) {
+        out << "Random\n";
+    }
+
+    for (int i = 0; i < typesAlgo.size(); i++) {
+        out << "алгоритм - ";
+
+        if (typesAlgo[i] == RLS) {
+            out << "RLS\n";
+        } else if (typesAlgo[i] == ONEPLUSONE) {
+            out << "(1 + 1)\n";
+        } else if (typesAlgo[i] == LAMBDA) {
+            out << "(1 + (лямбда, лямбда))\n";
+        }
+
+        out << "время работы: ";
+
+        for (int j = 0; j < REPEAT; j++) {
+            out << result[i][j] << ' ';
+        }
+        out << '\n';
+        out << "границы - [" << min[i] << ", " << max[i] << "]\n";
+
+        out << "среднее время работы - " << average[i] << '\n';
+        out << "нормированное по размеру задачи - " << (double) average[i] / size << '\n';
+
+    }
+    out << "----------------------------\n";
 
 }
 
@@ -86,7 +182,7 @@ int main() {
 
     vector<int> sizeGraph {2048}; //{32, 64, 128, 256, 512, 1024, 2048}
     vector<TypeAlgo> typesAlgo = {RLS, ONEPLUSONE, LAMBDA}; //{RLS, ONEPLUSONE, LAMBDA}
-    vector<TypeGraph> typesGraph = {KN};  //{KNN, KN, RANDOM}
+    vector<TypeGraph> typesGraph = {RANDOM};  //{KNN, KN, RANDOM}
 
 
     for (int i = 0; i < typesGraph.size(); i++) {
